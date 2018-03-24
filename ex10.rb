6af4357391154bf80d2b7fe3626774a2b7d56144
Elle H <itsellej@gmail.com>
2018-03-24 14:16:49 +0000

    Complete exercise 10

---
+++ ex10.rb
@@ -0,0 +1,22 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+puts tabby_cat
+puts persian_cat
+puts backslash_cat
+puts fat_cat
+
+puts "This is a test.\nDid you know that this is a test?\nOh, did I mention that this is a test?"
+
+puts "This is a list:\n
+\t* Item 1
+\t* Item 2
+\t* Item 3"
