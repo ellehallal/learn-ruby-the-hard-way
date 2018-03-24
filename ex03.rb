27db910fe05d5f980090909db1da46b184a52a00
Elle H <itsellej@gmail.com>
2018-03-24 12:24:32 +0000

    Complete exercise 3

---
+++ ex03.rb
@@ -0,0 +1,31 @@
+#prints to the console
+puts "I will now count my chickens:"
+
+#prints text and then result of calculation
+puts "Hens #{25.0 + 30.0 / 6.0}"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+#prints to the console
+puts "Now I will count the eggs:"
+
+#prints result of calculation
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+#prints to the console
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+#returns true or false
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+#prints text and then result of calculation
+puts "What is 3 + 2? #{3.0 + 2.0}"
+puts "What is 5 - 7? #{5.0 - 7.0}"
+
+#prints to the console
+puts "Oh, that's why it's false."
+puts "How about some more?"
+
+#prints text and returns true or false in regards to calculation
+puts "Is it greater? #{5.0 > -2.0}"
+puts "Is it greater, or equal? #{5.0 >= -2.0}"
+puts "Is it less or equal? #{5.0 <= -2.0}"
