6a42cc9acb5298f051b90144d157217cb1fc1fce
Elle H <itsellej@gmail.com>
2018-03-24 15:25:01 +0000

    Amend exercise 12

--- ex12.rb
+++ ex12.rb
@@ -1,17 +1,17 @@
-# print "Give me a number: "
-# number = gets.chomp.to_i
-#
-# bigger = number.to_f * 100
-# puts "#{bigger} is a bigger number"
-#
-# print "Give me another number: "
-# another = gets.chomp
-# number = another.to_i
-#
-# smaller = number.to_f / 100
-# puts "#{smaller} is a smaller number"
+print "Give me a number: "
+number = gets.chomp.to_f
+
+bigger = number * 100
+puts "#{bigger} is a bigger number"
+
+print "Give me another number: "
+another = gets.chomp
+number = another.to_f
+
+smaller = number / 100
+puts "#{smaller} is a smaller number"
 
 puts "May I have some money please?"
 money = gets.chomp.to_f
 
-puts "Here is your change: #{(money / 10)}"
+puts "Here is your change: #{(money / 10).round(2)}"
