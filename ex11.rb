bc61431957ad46db8cc43d19bd0118dd46652b1e
Elle H <itsellej@gmail.com>
2018-03-24 14:59:25 +0000

    Complete exercise 11

---
+++ ex11.rb
@@ -0,0 +1,17 @@
+# print "How old are you? "
+# age = gets.chomp
+# print "How tall are you? "
+# height = gets.chomp
+# print "How much do you weigh? "
+# weight = gets.chomp
+#
+# puts "So you're #{age} years old, #{height} tall and you weigh #{weight}."
+
+puts "What's your first name? "
+first_name = gets.chomp
+puts "What's your middle name? "
+middle_name = gets.chomp
+puts "What's your surname? "
+last_name = gets.chomp
+
+puts "Nice to meet you, #{first_name} #{middle_name} #{last_name}!"
