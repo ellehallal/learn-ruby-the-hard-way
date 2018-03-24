7f927535bdb0e7c4f264990b1646aca712cc4a04
Elle H <itsellej@gmail.com>
2018-03-24 15:23:16 +0000

    Complete exercise 12

---
+++ ex12.rb
@@ -0,0 +1,17 @@
+# print "Give me a number: "
+# number = gets.chomp.to_i
+#
+# bigger = number.to_f * 100
+# puts "#{bigger} is a bigger number"
+#
+# print "Give me another number: "
+# another = gets.chomp
+# number = another.to_i
+#
+# smaller = number.to_f / 100
+# puts "#{smaller} is a smaller number"
+
+puts "May I have some money please?"
+money = gets.chomp.to_f
+
+puts "Here is your change: #{(money / 10)}"
