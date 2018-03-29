5e333a3ffb30e2e9db665b08bf2ce4682e929ca8
Elle H <itsellej@gmail.com>
2018-03-29 14:15:47 +0100

    Complete exercise 34

---
+++ ex34.rb
@@ -0,0 +1,41 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+=begin
+
+The animal at 1.
+ruby
+
+The third (3rd) animal.
+peacock
+
+The first (1st) animal.
+bear
+
+The animal at 3.
+kangaroo
+
+The fifth (5th) animal.
+whale
+
+The animal at 2.
+peacock
+
+The sixth (6th) animal.
+platypus
+
+The animal at 4.
+whale
+
+=end
+
+food = ["ice cream", "blueberries", "broccoli", "katsu curry", "victoria sponge cake"]
+
+=begin
+The array contains 5 items
+Ice cream is at index 0 - food[0]
+Blueberries is at index 1 - food[1]
+Broccoli is at index 2 - food[2]
+katsu curry is at index 3 - food[3]
+victoria sponge cake is at index 4 - food[4]
+
+=end
