4a1895d582e779e86d68f01e0f58455426d82e90
Elle H <itsellej@gmail.com>
2018-05-06 12:06:40 +0100

    Create ex01

--- practise/ex01.rb
+++ practise/ex01.rb
@@ -1,7 +1,19 @@
+=begin
 puts "Hello World!"
 puts "Hello Again"
 puts "I like typing this"
 puts "This is fun"
 puts "Yay! Printing."
 puts "I'd much rather you 'not'"
+=end
 puts 'I "said" do not touch this'
+puts "Print this additional line"
+
+
+=begin
+For this exercise, try these things:
+
+- Make your script print another line.
+- Make your script print only one of the lines.
+- Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does.
+=end
