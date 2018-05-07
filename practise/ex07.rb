7df05c1244afdd906421a2d18e0ec8c543e8852a
Elle H <itsellej@gmail.com>
2018-05-07 10:38:27 +0100

    Complete ex07

---
+++ practise/ex07.rb
@@ -0,0 +1,36 @@
+
+#print string
+puts "Mary had a little lamb"
+#print string, but 'snow' seems to be considered a string
+puts "Its fleece was white as #{'snow'}"
+#print string
+puts "And everywhere that Mary went"
+#print . 10 times
+puts "." * 10
+
+#Single characters as a string assigned to variables"
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
+
+=begin
+
+Study Drills
+For these next few exercises, you will have the exact same Study Drills.
+
+Go back through and write a comment on what each line does. - done
+Read each one backward or out loud to find your errors. - done
+
+=end
