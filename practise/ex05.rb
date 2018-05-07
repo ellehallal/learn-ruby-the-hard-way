745543efc24ff450111e31907c82fbad5ed416b5
Elle H <itsellej@gmail.com>
2018-05-07 09:57:22 +0100

    Create ex06

--- practise/ex05.rb
+++ practise/ex05.rb
@@ -21,11 +21,8 @@ puts "Her weight in kilograms is #{pounds_to_kg.round}"
 puts "Her height in centimeters is #{inches_to_cm.round}"
 
 =begin
-
 Study Drills
 - Change all the variables so there is no my_ in front of each one. Make sure you change the name everywhere, not just where you used = to set them. - done
 
 - Try to write some variables that convert the inches and pounds to centimeters and kilograms. Do not just type in the measurements. Work out the math in Ruby. - done
-
-
 =end
