ec24e4322de466210a1d8f75e3abbf370485deb5
Elle H <itsellej@gmail.com>
2018-05-13 08:31:40 +0100

    Create ex11-form

---
+++ practise/ex11-form.rb
@@ -0,0 +1,13 @@
+print "What's your name?"
+name = gets.chomp
+
+print "Where do you live?"
+location = gets.chomp
+
+print "What is your occupation?"
+occupation = gets.chomp
+
+print "What's your favourite colour"
+fav_colour = gets.chomp
+
+puts ""
