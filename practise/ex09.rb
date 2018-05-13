0769ae388b623cf8beccaeb93779f0d55f9ab8fe
Elle H <itsellej@gmail.com>
2018-05-13 08:06:46 +0100

    Complete ex09

--- practise/ex09.rb
+++ practise/ex09.rb
@@ -1,12 +1,26 @@
+#assign string to days variable
 days = "Mon Tue Wed Thu Fri Sat Sun"
+#assign string to months variable. /n creates a new line between each month
 months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
 
+#printing string with value of variables
 puts "Here are the days: #{days}"
 puts "Here are the months: #{months}"
 
+#a multi line string
 puts %q{
 There's something going on here.
 With this weird quote
 We'll be able to type as much as we like.
 Even 4 lines if we want, or 5, or 6.
 }
+
+
+=begin
+
+Study Drills
+Go back through and write a comment on what each line does.
+Read each one backward or out loud to find your errors.
+From now on, when you make mistakes, write down on a piece of paper what kind of mistake you made.
+
+ = end
