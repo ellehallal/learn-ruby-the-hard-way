aea5fd3f108beabe11c28d2b1dcc33a4ea0209bd
Elle H <itsellej@gmail.com>
2018-05-13 08:35:00 +0100

    Complete ex11 and ex11-form

--- practise/ex11-form.rb
+++ practise/ex11-form.rb
@@ -1,13 +1,18 @@
-print "What's your name?"
+print "What's your name? "
 name = gets.chomp
 
-print "Where do you live?"
+print "Where do you live? "
 location = gets.chomp
 
-print "What is your occupation?"
+print "What is your occupation? "
 occupation = gets.chomp
 
-print "What's your favourite colour"
+print "What's your favourite colour? "
 fav_colour = gets.chomp
 
-puts ""
+puts """
+  Hello #{name}! Nice to meet you.
+  You live in #{location}. That's a nice area.
+  Do you enjoy your job working as a #{occupation}?
+  I like the colour #{fav_colour} too!
+"""
--- practise/ex11.rb
+++ practise/ex11.rb
@@ -11,7 +11,16 @@ puts "You are #{age} years old and your height is #{height}. You weigh #{weight}
 
 =begin
 Study Drills
-- Go online and find out what Ruby's gets.chomp does.
+- Go online and find out what Ruby's gets.chomp does
+* Two methods
+* gets = prompts the user to input text. Also adding a new line to the end of your string
+* chomp = method that can be called on a string. Strips text of any new lines or carriage returns
+  at the end
+  source: http://discourse.codenewbie.org/t/what-does-gets-chomp-do/559
+
 - Can you find other ways to use it? Try some of the samples you find.
+* gets.strip - removes trailing whitespace
+
 - Write another "form" like this to ask some other questions.
+* see ex11-form
 =end
