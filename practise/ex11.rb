00bd9a59039ad129a55fe3f2190dd3e8efbf3e43
Elle H <itsellej@gmail.com>
2018-05-13 08:19:39 +0100

    Create ex11.rb

---
+++ practise/ex11.rb
@@ -0,0 +1,17 @@
+print "How old are you? "
+age = gets.chomp
+
+print "How tall are you? "
+height = gets.chomp
+
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "You are #{age} years old and your height is #{height}. You weigh #{weight}."
+
+=begin
+Study Drills
+- Go online and find out what Ruby's gets.chomp does.
+- Can you find other ways to use it? Try some of the samples you find.
+- Write another "form" like this to ask some other questions.
+=end
