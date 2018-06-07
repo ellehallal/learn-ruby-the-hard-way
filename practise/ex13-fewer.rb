f3a1201f9b11a619ca9f2ac5e093faac478eb71a
Elle H <itsellej@gmail.com>
2018-06-07 13:32:04 +0100

    Complete fewer and more scripts for ex13

---
+++ practise/ex13-fewer.rb
@@ -0,0 +1,4 @@
+first, second = ARGV
+
+puts "This is my first variable: #{first}"
+puts "This is my second variable: #{second}"
---
+++ practise/ex13-more.rb
@@ -0,0 +1,7 @@
+first, second, third, fourth, fifth = ARGV
+
+puts "This is my first variable: #{first}"
+puts "This is my second variable: #{second}"
+puts "This is my third variable: #{third}"
+puts "This is my fourth variable: #{fourth}"
+puts "This is my fifth variable: #{fifth}"
